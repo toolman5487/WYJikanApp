fd03160be3402d3baed320e9906f5d6f5c23c77b
Willy Hsu <hsunefer@gmail.com>
2026-03-24 11:18:49 +0800

    Initial Commit

---
+++ WYJikanApp/WYJikanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WYJikanAppApp.swift
+//  WYJikanApp
+//
+//  Created by Willy Hsu on 2026/3/24.
+//
+
+import SwiftUI
+
+@main
+struct WYJikanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
